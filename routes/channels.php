d25c50db2bf093ebc1e723b9637f574a5695490b
manh <aumanh11112004@gmail.com>
2026-05-07 01:42:41 +0700

    sửa lại cho linux

--- routes/channels.php
+++ routes/channels.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Broadcast;
 use App\Models\Participant;
 
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 Broadcast::channel('chat.{conversationId}', function ($user, $conversationId) {
     // Kiểm tra xem user có phải là thành viên của phòng chat này không
     return Participant::where('conversation_id', $conversationId)
